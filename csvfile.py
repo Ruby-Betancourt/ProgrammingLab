071bbdad4876261255288c7e259dac4fd42d0859
Ruby-Betancourt <92112439+Ruby-Betancourt@users.noreply.github.com>
2021-11-26 15:35:56 +0000

    es 26/11

--- csvfile.py
+++ csvfile.py
@@ -1,27 +1,60 @@
 class CSVFile():
   #inizializzo la classe
-  def __init__(self, name):
-    self.name = name
+    def __init__(self, name):
+         self.name = name
 
-  def get_data(self):
-    #inizializzo futura la lista di liste
-    finish_list = []
-    #
-    my_file = open('sales.txt', 'r')
-    for line in my_file:
-      elements = line.split(',')
+    def get_data(self):
+        #inizializzo futura la lista di liste
+        finish_list = []
 
-      if elements[0] != 'Date':
-        data = elements[0]
-        value = elements[1]
+        #apro il file txt
+        #my_file = open('sales.txt', 'r')
 
-        first_list = [elements[0], elements[1]]
-        finish_list.append(float(element))    
-    my_file.close()
-    return finish_list
+        try:
+            my_file = open('sale.txt', 'r')
+        except FileNotFoundError:
+            print('Non esiste il file "sale.txt"') 
+            print('il file giusto da usare si chiama "sales.txt"') 
+            my_file = open('sales.txt', 'r')
 
-mio_file = CSVFile('sales.txt')
-mio_file.get_data()
 
-    
+        for line in my_file:
+            #split gli element 
+            elements = line.split(',')
+            #aggiungo ogni lista nella lista finale
+            if elements[0] != 'Date':
+                finish_list.append(elements)  
+        
+        #chiudo il file e return la lista di liste
+        my_file.close()
+        return finish_list
+
+
+my_file = CSVFile('sales.txt')
+print(my_file.get_data())
+
+class NumericalCSVFile(CSVFile):
+    pass
+
+    def get_data(self):
+        data = super().get_data()  
+        use = []
+
+
+        for item in data:
+
+            for x in item[1:]:
+                try:
+                    x = float(x)
+                    use.append(x)
+                except ValueError:    
+                    print('il tipo dell\'item è: {}'.format(type(x)))
+
+        return use            
+
+file_p2 = NumericalCSVFile('sales.txt')
+print(file_p2.get_data())
+
+
+
             
--- somma_liste.py
+++ somma_liste.py
@@ -1,8 +1,6 @@
-""" commento?? """
-
-
 #somma gli esementi di una lista
 def somma_liste(the_list):
+  #inizializzo la variabile somma
   som=0
   for number in the_list:
     som = som + number  
